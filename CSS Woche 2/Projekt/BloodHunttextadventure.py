5fc87517d290a6270b78a61ade927e0b22de3d0a
FerenzPlayz <davidhertzstein@outlook.com>
2025-12-01 16:25:12 +0100

    Refactor game logic and update music paths

--- CSS Woche 2/Projekt/BloodHunttextadventure.py
+++ CSS Woche 2/Projekt/BloodHunttextadventure.py
@@ -1,6 +1,6 @@
-import sys
-import time
 import pygame
+import time
+import sys
 
 def langsamer_text(text, delay = 0.03):
     for char in text:
@@ -10,7 +10,7 @@ def langsamer_text(text, delay = 0.03):
 
 
 def main_menu():
-    pygame.mixer.music.load("Projekt/mixkit-tapis-615.mp3")
+    pygame.mixer.music.load("Projekt-Tag/mixkit-tapis-615.mp3")
     pygame.mixer.music.play(-1)
     while True:
         print("====================================================================")
@@ -48,37 +48,43 @@ def starte_spiel():
     langsamer_text("Aria... einst eine friedliche Stadt voller Musik und Blumen.")
     langsamer_text("Doch diese Zeiten sind vorbei.")
     langsamer_text("Ein süßer, unheilvoller Rosenduft liegt über den Straßen.")
-    langsamer_text("Menschen verschwinden – und wer zurückkehrt, ist von Dornen umhüllt.")
-    langsamer_text("Die Finstere Rosenkönigin hat ihr Dornenschloss über das Land ausgebreitet.")
+    langsamer_text("Menschen verschwinden und wer zurückkehrt, ist von Dornen umhüllt.")
+    langsamer_text("Die Finstere Rosenkönigin Rosamira hat ihr Dornenschloss über das Land ausgebreitet.")
     langsamer_text("Lebende Ranken wuchern durch die Stadt, verwandeln Menschen in Kreaturen.")
-    langsamer_text("\nDu bist einer der letzten, die noch frei sind.")
-    langsamer_text("Dein Amulett schützt dich – doch nicht für immer.")
+    langsamer_text("\nDu bist der letzte Überlebende")
     langsamer_text("\nDeine Mission:")
     langsamer_text(" - Die Stadt Aria retten")
     langsamer_text(" - Die Dornen vernichten")
     langsamer_text(" - Die Rosenkönigin töten\n")
-    langsamer_text("Das Schicksal der Menschheit liegt in deinen Händen...\n")
+    langsamer_text("Das Schicksal der Menschheit liegt in deinen Händen... Bitte... \n rette uns...\n")
+    charakterauswahl()
 
-
-def vor_Dem_tor():
-   langsamer_text("Aria... einst eine friedliche Stadt voller Musik und Blumen.")
-   langsamer_text("Doch diese Zeiten sind vorbei.")
-   langsamer_text("Ein süßer, unheilvoller Rosenduft liegt über den Straßen.")
-   langsamer_text("Menschen verschwinden – und wer zurückkehrt, ist von Dornen umhüllt.")
-   langsamer_text("Die Finstere Rosenkönigin hat ihr Dornenschloss über das Land ausgebreitet.")
-   langsamer_text("Lebende Ranken wuchern durch die Stadt, verwandeln Menschen in Kreaturen.")
-   langsamer_text("\nDu bist einer der letzten, die noch frei sind.")
-   langsamer_text("Dein Amulett schützt dich – doch nicht für immer.")
-   langsamer_text("\nDeine Mission:")
-   langsamer_text(" - Die Stadt Aria retten")
-   langsamer_text(" - Die Dornen vernichten")
-   langsamer_text(" - Die Rosenkönigin töten\n")
-   langsamer_text("Das Schicksal der Menschheit liegt in deinen Händen...\n")
-   
-   langsamer_text("Du stehst im Foyer...")
+def charakterauswahl():
+    pygame.mixer.music.load("Projekt-Tag/intense-rain-city-night-171461.mp3")
+    pygame.mixer.music.play(-1)
+    langsamer_text("Du bist da... Du siehst nichts...")
+    langsamer_text("Das Einzige was du hörst, ist der Klang des Regens.. ")
+    langsamer_text("Du weißt nicht mehr wer du bist..")
+    langsamer_text("Doch du kannst dich erinnern was du bist..")
+    charakterwahl = input("Also was bist du?? gib ein (Krieger, Magier , Assasine): ")
+    if charakterwahl == "Krieger":
+        langsamer_text(f"Du bist ein {charakterwahl} ")
+    elif charakterwahl == "Magier":
+        langsamer_text(f"Du bist ein {charakterwahl} ")
+    elif charakterwahl == "Assasine":
+        langsamer_text(f"Du bist ein {charakterwahl} ")
+    langsamer_text(f"So mein {charakterwahl} WACH AUF!!!!")
+    
+def Vordemtor():
+    langsamer_text("Du wachst vor dem Dornenschloss auf. Es regnet nach wie vor..")
+    langsamer_text("Du stehst langsam auf... Das Tor zum Dornenschloss ist offen..")
+    wahl2 = input("Willst du reingehen (Ja / Nein)")
+    if wahl2.lower() == "wahl":
+        print("")
 
 
 pygame.init()
 pygame.mixer.init()
 main_menu()
+
      
